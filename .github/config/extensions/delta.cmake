bbafcb3b7c58accb6dbb82260ba9dac02db24dec
ben fleis <benfleis@users.noreply.github.com>
2026-04-09 10:31:17 +0200

    bump azure and delta for v1.5.2

--- .github/config/extensions/delta.cmake
+++ .github/config/extensions/delta.cmake
@@ -1,7 +1,7 @@
 if(NOT MINGW AND NOT ${WASM_ENABLED})
   duckdb_extension_load(delta
             GIT_URL https://github.com/duckdb/duckdb-delta
-            GIT_TAG 380933c093f1af67515831e9b80fe8b690ee8d8a
+            GIT_TAG 776be7ac2c51211e12bcd46c911ab11267cc44f3
             SUBMODULES extension-ci-tools
             APPLY_PATCHES
   )
